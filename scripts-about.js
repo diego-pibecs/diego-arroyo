d74146ab059ee332860aa2191249ab1dbf3cccc8
Diego <diego.pibecs@gmail.com>
2023-03-20 19:45:25 -0600

    Arreglos menores

--- scripts-about.js
+++ scripts-about.js
@@ -1,4 +1,4 @@
-
+let comesFrom = 0;
 aboutMeBtn = document.getElementById("about-me-btn");
 aboutMeBtn.addEventListener("click", aboutMeTransition);
 
@@ -11,8 +11,15 @@ currentlyReadingBtn.addEventListener("click", () => {
     }, 2000)
 })
 
-
 function aboutMeTransition() {
+    let comesFromAbout = 0;
+
+    let contactBtn = document.getElementById("contact-btn");
+    contactBtn.removeEventListener("click", contactTransition);
+    contactBtn.addEventListener("click", function () {
+        contactTransition(comesFromAbout);
+    });
+
     let navMenuIcon = document.getElementById("nav-menuicon");
     navMenuIcon.style.fill = "azure";
 
@@ -150,8 +157,9 @@ function aboutMeTransition() {
             location.reload();
         });
     }
-}
 
+    comesFromAbout == 2;
+}
 
 let activeIndex = 0;
 const sections = document.getElementsByClassName("about-me-section");
@@ -164,7 +172,7 @@ const handleRightClick = () => {
 
     if (activeIndex == 2) {
         sign.classList.remove('hideSign');
-        sign.classList.add('showSign')
+        sign.classList.add('showSign');
     } else if (activeIndex == 1 || activeIndex == 0) {
         sign.classList.remove('showSign');
         sign.classList.add('hideSign');
@@ -199,4 +207,89 @@ const handleLeftClick = () => {
         nextSection.dataset.status = "active";
         activeIndex = nextIndex;
     });
+}
+
+
+// Para contact
+let comesFromHome = 1;
+contactBtn = document.getElementById("contact-btn");
+contactBtn.addEventListener("click", function () {
+    contactTransition(comesFromHome);
+});
+
+function contactTransition(comesFrom) {
+    if (comesFrom == 0) {
+        mainTitle = document.getElementById("main-title-sign");
+        mainTitle.classList.remove("change-title");
+        mainTitle.classList.remove("change-title-contact");
+        mainTitle.classList.add("changing-from-about");
+
+        let containers = document.querySelectorAll(".about-me-container");
+        containers.forEach(container => {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+        });
+
+        signWhite = document.getElementById("firma-white");
+        signWhite.classList.add("hideSign");
+    }
+
+    signBlack = document.getElementById("firma-black");
+    signBlack.classList.remove('hide');
+
+    let navMenuIcon = document.getElementById("nav-menuicon");
+    navMenuIcon.removeEventListener('click', displayMenu);
+    navMenuIcon.removeEventListener('click', transitionMenuIcon);
+
+    navMenuIcon.setAttribute('id', 'new-menu-icon');
+    let newMenuIcon = document.getElementById("new-menu-icon");
+    newMenuIcon.style.fill = "black";
+    newMenuIcon.firstElementChild.setAttribute("d", "M17.026 22.957c10.957-11.421-2.326-20.865-10.384-13.309l2.464 2.352h-9.106v-8.947l2.232 2.229c14.794-13.203 31.51 7.051 14.794 17.675z");
+    newMenuIcon.addEventListener("click", () => {
+        location.reload();
+    });
+
+
+    document.getElementById("main-wrapper").scrollIntoView({behavior: "smooth"})
+
+    mainArticle = document.getElementById("main-article");
+    mainArticle.style.display = "none";
+
+    mainWrapper = document.getElementById("main-wrapper");
+    mainWrapper.style.backgroundImage = "none"
+
+    mainTitle = document.getElementById("main-title-sign");
+    if (comesFrom != 0) {
+        mainTitle.classList.add("change-title-contact");
+    }
+
+    mainSubtitle = document.getElementById("main-title-sub");
+    mainSubtitle.classList.add("hide");
+
+    const bodyTag = document.getElementsByTagName("body");
+    for (let index = 0; index < bodyTag.length; index++) {
+        const body = bodyTag[index];
+        body.style.backgroundImage = "none";
+        body.style.backgroundColor = "white";
+        body.style.backgroundRepeat = "no-repeat";
+        body.style.backgroundSize = "cover";
+        body.style.height = "100vh";
+        body.style.overflow = "hidden";
+    }
+
+    mainWrapperSection = document.getElementById("main-wrapper-section");
+    mainWrapperSection.classList.add("hide-smooth");
+
+    contactText = document.getElementsByClassName("text-contact");
+    for (let index = 0; index < contactText.length; index++) {
+        const text = contactText[index];
+        text.classList.add("show-contact");
+    }
+
+    contactTextContainer = document.getElementsByClassName("text-contact-container");
+    for (let index = 0; index < contactTextContainer.length; index++) {
+        const textContainer = contactTextContainer[index];
+        textContainer.style.display = "flex";
+    }
 }
